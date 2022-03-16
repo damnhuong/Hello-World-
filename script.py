e49dda404d5b300b64655560c5f5e94bb157cef0
damnhuong <93033728+damnhuong@users.noreply.github.com>
2022-03-16 10:53:53 +0700

    Create script.py

---
+++ script.py
@@ -0,0 +1,9 @@
+num1=(input("Enter number 1:"))
+num2=(input("Enter number 2: "))
+try:
+  float(number_1)=num1
+  float(number_2)=num2
+	tong=number_1 + number_2
+	print("Tong of two number:",tong)
+except:
+	print("Error!\nRun again program)
